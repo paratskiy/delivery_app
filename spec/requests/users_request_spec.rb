c23962ac7a9dc3013fec2f130fdec876e85cb294
Bogdan Paratskiy <bog4242@gmail.com>
2020-05-09 19:59:59 +0300

    Finish user signup

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -6,5 +6,22 @@ RSpec.describe 'Users', type: :request do
       get signup_path
       expect(response).to have_http_status(:success)
     end
+
+    it 'invalid signup information' do
+      user_params = FactoryBot.attributes_for(:user)
+      user_params[:name] = ''
+      get signup_path
+      expect(response).to render_template(:new)
+      post users_path, params: { user: user_params }
+      assert_template 'users/new'
+    end
+
+    it 'creates a User and redirects to the User page' do
+      get signup_path
+      expect(response).to render_template(:new)
+      post users_path, params: { user: FactoryBot.attributes_for(:user) }
+      expect(User.count).to eq(1)
+      expect(response).to redirect_to(assigns(:user))
+    end
   end
 end
