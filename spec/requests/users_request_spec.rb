4fb14097cc26016a95be9f0ad4b290b8c7436456
Bogdan Paratskiy <bog4242@gmail.com>
2020-07-16 20:45:09 +0300

    add specs for links

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
+  let(:user) { FactoryBot.create(:user) }
+
   describe 'GET /new' do
     it 'returns http success' do
       get signup_path
@@ -25,6 +27,28 @@ RSpec.describe 'Users', type: :request do
       expect(User.count).to eq(1)
       expect(response).to redirect_to(assigns(:user))
       expect(flash[:success]).to match('Welcome to Delivery App!')
+    end
+  end
+
+  describe 'login' do
+    it 'login with valid information followed by logout' do
+      get login_path
+      post login_path, params: { session: { email: user.email,
+                                            password: user.password } }
+      expect(is_logged_in?).to be true
+      expect(response).to redirect_to user
+      follow_redirect!
+      expect(response).to render_template('users/show')
+      assert_select 'a[href=?]', login_path, count: 0
+      assert_select 'a[href=?]', logout_path
+      assert_select 'a[href=?]', user_path(user)
+      delete logout_path
+      expect(is_logged_in?).to be false
+      expect(response).to redirect_to root_url
+      follow_redirect!
+      assert_select 'a[href=?]', login_path
+      assert_select 'a[href=?]', logout_path, count: 0
+      assert_select 'a[href=?]', user_path(user), count: 0
     end
   end
 end
